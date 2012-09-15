58ae66eecdcd23ceabcfc1deb15532bdf643db19
Ben van Enckevort <benvan@gmail.com>
2012-09-15 21:33:12 +0100

    working ticker

---
+++ js/jquery.ticker.js
@@ -0,0 +1,26 @@
+(function ($) {
+    $.fn.ticker = function () {
+        return this.each(function () {
+            var el = $(this);
+
+            $.getJSON("ticker.json", "", function (data) {
+                el.html("");
+                $(data.systems).each(function (i, sys) {
+                    el.append(
+                        $("<li></li>")
+                            .addClass("elem")
+                            .append(
+                            $('<a href="' + sys.url + '"></a>').append(
+                                $('<div></div>')
+                                    .append('<img src="img/100x100.gif" alt="">')
+                                    .append('<div class="elem-label ellipsis">' + sys.name + '</div>')
+                            ))
+                    );
+                });
+                $(".ellipsis").ellipsis();
+            });
+
+
+        });
+    };
+})(jQuery);
