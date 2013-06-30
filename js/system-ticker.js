90f291a0ce92537311652eaefb1b0ea530748b2e
Ben van Enckevort <benvan@gmail.com>
2013-06-30 18:46:40 +0100

    got rid of system-ticker cruft

--- js/system-ticker.js
+++ js/system-ticker.js
@@ -13,18 +13,13 @@
                             $('<a href="' + sys.url + '"></a>')
                                 .click(function(){
                                     $("#systemName").html(sys.name);
-                                    $("#systemInfo").addClass("blue").slideDown();
                                 })
-                                .append(
-                                    $('<div class="elem-author"></div>').append(sys.author)
-                                )
                                 .append( $('<div class="img-holder"></div>')
                                     .append('<img src="img/'+ (sys.img ? "systems/"+sys.img : "100x100.gif")+'" alt="">')
-                                    .append('<div class="elem-label ellipsis">' + sys.name + '</div>')
+                                    .append('<div class="elem-label">' + sys.name + '</div>')
                             ))
                     );
                 });
-                $(".ellipsis").ellipsis();
             });
 
         });
